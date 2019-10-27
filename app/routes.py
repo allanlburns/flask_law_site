3162a282c111ce92cae73580f781020760ba1686
Allan Burns <allanlburns@yahoo.com>
2019-10-27 16:30:20 -0400

    Routes, templates, and styles working for all templates. TODO: Contact form, database, clean up styling on locations.html.

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,19 @@ from flask import render_template, url_for, redirect
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/mission')
+def mission():
+    return render_template('mission.html')
+
+@app.route('/locations')
+def locations():
+    return render_template('locations.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
