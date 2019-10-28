9263ec60b59a7ddae02045c99fc156c29db6eca7
Allan Burns <allanlburns@yahoo.com>
2019-10-27 20:43:42 -0400

    Created login.html, login form, and login route. Added database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from config import Config
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # import routes goes at bottom to solve circular routes problem
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, flash
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +22,12 @@ def locations():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
