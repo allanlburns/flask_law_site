1cbf2e79972607d55a49439761c47b1b14ab37dd
Allan Burns <allanlburns@yahoo.com>
2019-10-28 20:51:55 -0400

    Login, logout, and registration working.

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,15 @@ from config import Config
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 # import routes goes at bottom to solve circular routes problem
 from app import routes
